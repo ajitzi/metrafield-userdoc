cd7bb3d90b8b9174f12f324bd5627c3abb0e3cf5
undefined <undefined>
2025-10-23 20:47:42 +0200

    fix errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import {DocumentationApp} from "./index";
+import DocumentationApp from "./DocumentationApp";
 
 createRoot(document.getElementById("root")!).render(<DocumentationApp />);
   
